51b485b22de9dff0a9c59fabd7a258e7e8a011a6
marleeeeeey <marleeeeeey@gmail.com>
2024-05-31 02:14:03 +0400

    13 Apply gravity.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,21 +2,32 @@ extends CharacterBody2D
 
 var speed = 300.0
 
+var gravity = 15.0
+var max_fall_velocity = 1000.0
+
 var viewport_size
 
-func _ready() -> void:
+func _ready():
 	viewport_size = get_viewport_rect().size 
 
-func _process(delta: float) -> void:
+func _process(delta: float):
 	pass
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta: float):
+	
+	# Apply gravity.
+	velocity.y += gravity
+	if velocity.y > max_fall_velocity:
+		velocity.y = max_fall_velocity
+	print(velocity.y)
+	
+	
+	# Control left-right movement.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed/10)
-		
 	move_and_slide()
 	
 	# Teleport over the screen.
