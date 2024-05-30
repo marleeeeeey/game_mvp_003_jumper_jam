2331557f1ca0d644dd05eebf4c39bf4288149101
marleeeeeey <marleeeeeey@gmail.com>
2024-05-31 02:37:18 +0400

    14 Player jump bounce from platforms.

--- scripts/platform.gd
+++ scripts/platform.gd
@@ -2,4 +2,7 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body is Player:
-		print("player entered the platform")
+		
+		# If player goes down then it should jump on touch with platform.
+		if body.velocity.y > 0:
+			body.jump()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,26 +1,36 @@
 class_name Player
 extends CharacterBody2D
 
+@onready var animator = $AnimationPlayer
+
 var speed = 300.0
 
 var gravity = 15.0
 var max_fall_velocity = 1000.0
+var jump_velocity = -800
 
 var viewport_size
+
 
 func _ready():
 	viewport_size = get_viewport_rect().size 
 
 func _process(delta: float):
-	pass
+	
+	# Change animation when falling and jumping.
+	if velocity.y > 0:
+		if animator.current_animation != "fall":
+			animator.play("fall")
+	elif velocity.y < 0:
+		if animator.current_animation != "jump":
+			animator.play("jump")
 
 func _physics_process(delta: float):
 	
 	# Apply gravity.
 	velocity.y += gravity
 	if velocity.y > max_fall_velocity:
-		velocity.y = max_fall_velocity
-	
+		velocity.y = max_fall_velocity	
 	
 	# Control left-right movement.
 	var direction = Input.get_axis("move_left", "move_right")
@@ -36,4 +46,8 @@ func _physics_process(delta: float):
 		global_position.x = -margin
 	elif global_position.x < -margin:
 		global_position.x = viewport_size.x + margin
-	 
+ 
+
+func jump():
+	velocity.y = jump_velocity
+	
