fa01a8203c220921c0e593adb951b8a4e71b75fe
marleeeeeey <marleeeeeey@gmail.com>
2024-05-31 10:18:14 +0400

    18 Generate the ground.

--- scripts/game.gd
+++ scripts/game.gd
@@ -12,9 +12,15 @@ func _ready():
 	camera.setup_camera($Player)
 	add_child(camera)
 	
-	create_platform(Vector2(100, 100))
-	create_platform(Vector2(100, 500))
-	create_platform(Vector2(100, 800))
+	# Generate the ground.
+	var viewport_size = get_viewport_rect().size
+	var platform_width = 136 # Platform collision shape wight.
+	var ground_layer_platform_count = (viewport_size.x / platform_width) + 1	
+	var ground_layer_y_offset = 62 # PLatform sprite height.
+	for i in range(ground_layer_platform_count):
+		var ground_location = Vector2 (i * platform_width, viewport_size.y - ground_layer_y_offset)
+		create_platform(ground_location)
+
 
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
