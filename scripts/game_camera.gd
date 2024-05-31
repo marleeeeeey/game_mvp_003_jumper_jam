a9900749025a184e6c076c4b58de1112f1d5e6d5
marleeeeeey <marleeeeeey@gmail.com>
2024-05-31 11:37:08 +0400

    23. Create platform destroyer.

--- scripts/game_camera.gd
+++ scripts/game_camera.gd
@@ -3,17 +3,21 @@ extends Camera2D
 var player: Player = null
 
 @onready var viewport_size = get_viewport_rect().size
+@onready var destroyer = $Destroyer
+@onready var destroyer_shape = $Destroyer/CollisionShape2D
+
 
 func setup_camera(_player: Player):
 	if _player:
 		player = _player
 
+
 func _physics_process(delta):
 	if player:
 		global_position.y = player.global_position.y
 
+
 func _ready():
-	
 	# Centered the camera. It should be done code behind to support any screen resolutions.
 	global_position.x = viewport_size.x / 2
 
@@ -21,6 +25,13 @@ func _ready():
 	limit_bottom = viewport_size.y
 	limit_left = 0
 	limit_right = viewport_size.x
+
+	# Create platform destroyer.
+	destroyer.position.y = viewport_size.y / 2.0  # Position relative to camera.
+	var rect_shape = RectangleShape2D.new()
+	var rect_shape_size = Vector2(viewport_size.x, 200)
+	rect_shape.set_size(rect_shape_size)
+	destroyer_shape.shape = rect_shape
 
 
 func _process(delta):
