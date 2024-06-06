979cd97b4592b1302202df3fb4fd26bbc2407b28
marleeeeeey <marleeeeeey@gmail.com>
2024-06-06 13:11:30 +0400

    50. Apply iOS specific screen scale to safe area.

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -11,6 +11,12 @@ func _ready():
 		var safe_area = DisplayServer.get_display_safe_area()
 		var safe_area_top = safe_area.position.y
 
+		if os_name == "iOS":
+			# The method is callable in OS systems.
+			var screen_scale = DisplayServer.screen_get_scale()
+			safe_area_top = safe_area_top / screen_scale
+			MyUtility.add_log_msg("iOS screen_scale: " + str(screen_scale))
+
 		topbar.position.y += safe_area_top
 		var margin = 10
 		topbar_bg.size.y += safe_area_top + margin
